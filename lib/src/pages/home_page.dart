34f750fe9359ed476d33a25636e2f1193eb21f0f
Fredy Fuentes <fredy.fuentesb@gmail.com>
2020-06-21 10:18:27 -0500

    Aplicacion qr terminada

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/bloc/scans_bloc.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
+
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:qrreaderapp/src/utils/utils.dart' as utils;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final scansBloc = new ScansBloc();
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: scansBloc.borrarTodos,
+          )
+        ],
+      ),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearbottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: ()=>_scanQr(context),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  _scanQr(BuildContext context) async {
+    //https://fernando-herrera.com
+    //geo:40.72215149405802,-74.18446913203128
+    dynamic futureString ='';
+    try {
+      futureString = await BarcodeScanner.scan();
+    }catch(e){
+      futureString=e.toString();
+    }
+ 
+    //print('Future String: ${futureString.rawContent}');
+    if (futureString != null){
+      final scan = ScanModel(valor: futureString.rawContent);
+      scansBloc.agregarScan(scan);
+      if(Platform.isIOS){
+        Future.delayed(Duration(milliseconds: 750), (){
+          utils.abrirScan(context, scan);
+        });
+      } else{
+        utils.abrirScan(context, scan);
+      }
+    }
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+      default: return MapasPage();
+    }
+  }
+
+  Widget _crearbottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index){
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas')
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones')
+        ),
+      ],
+    );
+  }
+}
